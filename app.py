eb55b3b7aa6dd7b808cdd26746f4d377200dbfa2
olusegun.odewole <olusegun.odewole@gunsel.com.tr>
2023-01-16 09:57:56 +0200

    multiclass model workin

--- app.py
+++ app.py
@@ -22,29 +22,23 @@ def save_uploadedfile(uploadedfile):
      return uploaded_location#st.success("Saved File:{} to {}".format(uploadedfile.name, uploaded_location))
 
 def image_predict (image_file):
-    model_path = 'application/models/Tumor_VGG_model.h5'  
+    model_path = 'application/models/multi_3_brainTumor89.h5'  
     h5_model = load_model(model_path)
     image = cv2.imread(image_file)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image = cv2.resize(image, (224, 224)) 
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image = cv2.resize(image, (512, 512)) 
     image = np.array(image) / 255
     image = np.expand_dims(image, axis=0)
-    h5_prediction = h5_model.predict(image)  
-    print('Prediction from h5 model: {}'.format(h5_prediction))
-    print(h5_prediction)
-    probability = h5_prediction[0]
-    print("H5 Predictions:")
-    print (probability)
-    if probability[0] > 0.8:
-        brain_tumor_pred = str('%.2f' % (probability[0] * 100) + '% Brain Tumour-Present')
-        probability = (probability[0] * 100)
-    else:
-        brain_tumor_pred = str('%.2f' % ((1 - probability[0]) * 100) + '% No Brain Tumour')
-        probability = ((1 - probability[0]) * 100)
-    return  brain_tumor_pred
+    h5_prediction = h5_model.predict(image)
+    answer = np.argmax(h5_prediction)
+    classes = ['Meningioma tumor', 'Glioma tumor', 'Pituitary tumor']     
+    prediction =classes[int(answer)-1]
+    print ('Prediction is ', prediction )  
+    print('Prediction from h5 model: {}'.format(prediction))
+    return  prediction
          
 
-st.title("🦠 Brain Tumor Prediction App from CT Images")
+st.title("🦠 Brain Tumor Prediction App from MRI Images")
 
 
 
@@ -63,7 +57,7 @@ with col1:
             st.write(file_details)
 
             #View Uploaded Image
-            st.image(load_image(image),width=250)
+            st.image(load_image(image),width=512)
             #save image to disk
             saved = save_uploadedfile(image)
 
@@ -87,7 +81,7 @@ with col1:
 with col2:
 #with st.sidebar:    
     st.write("Developed by AI & IOT Lab https://iot.neu.edu.tr by Olusegun Odewole (oooladeleodewole(at)gmail)  ")
-    #st.header("Sample CT Image")
+    #st.header("Sample MRI Image")
     st.image( "https://res.cloudinary.com/segestic/image/upload/v1670497190/covid/images/Y99_ntvrog.jpg", width=300, caption='Sample CT-Scan Image')#width=400
 
 
